1d553b103e8e63a37e7ec95c312e7a69be8da399
William Arnback <32479398+Wlammm@users.noreply.github.com>
2022-12-02 10:13:56 +0100

    format

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,7 +5,10 @@ fn main() {
     let mut elf_calories = get_elf_calories();
 
     let elf_with_most_calories = find_elf_with_most_calories(&elf_calories);
-    println!("Elf with most calories is: {} with: {} calories.", elf_with_most_calories, elf_calories[elf_with_most_calories]);
+    println!(
+        "Elf with most calories is: {} with: {} calories.",
+        elf_with_most_calories, elf_calories[elf_with_most_calories]
+    );
 
     // PART 2
     let mut top3_calories_total = 0;
@@ -14,26 +17,29 @@ fn main() {
         top3_calories_total += elf_calories[index];
         elf_calories.remove(index);
     }
-    println!("The top 3 elves got a total of {} calories!", top3_calories_total);
+    println!(
+        "The top 3 elves got a total of {} calories!",
+        top3_calories_total
+    );
 }
 
 fn get_elf_calories() -> Vec<i32> {
     let input_bytes = include_bytes!("../input.txt");
     let input = String::from_utf8_lossy(input_bytes);
 
-    let lines : Vec<&str> = input.split('\n').collect();
+    let lines: Vec<&str> = input.split('\n').collect();
     let mut calories_per_elf: Vec<i32> = Vec::new();
 
     let mut current_calories = 0;
     for line in lines {
         let sanitized_line = line.replace('\r', "");
-        
+
         if sanitized_line.is_empty() {
             calories_per_elf.push(current_calories);
             current_calories = 0;
             continue;
         }
-        
+
         let line_value: i32 = sanitized_line.parse::<i32>().expect("Failed to parse line");
         current_calories += line_value;
     }
@@ -46,7 +52,7 @@ fn find_elf_with_most_calories(elf_calories: &Vec<i32>) -> usize {
         .iter()
         .enumerate()
         .max_by(|(_, a), (_, b)| a.cmp(b))
-        .map(|(index, _)|index);
+        .map(|(index, _)| index);
 
     index.expect("Entered empty vector.")
 }
