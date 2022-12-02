c0ed5e07258352cb8ad31ec0ba392bed77aa0226
William Arnback <32479398+Wlammm@users.noreply.github.com>
2022-12-02 22:12:29 +0100

    day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,11 +7,57 @@ fn main() {
     let input = get_input();
 
     let mut score = 0;
-    for x in input {
+    for x in &input {
         score += calculate_score(x.as_str());
     }
 
-    println!("{}", score);
+    println!("First part: {}", score);
+
+    // part 2
+    let mut score2 = 0;
+    for x in &input {
+        score2 += calculate_score_second_part(x.as_str());
+    }
+
+    println!("Second part: {}", score2);
+}
+
+fn get_winning_char(input: char) -> char {
+    match input {
+        'A' => return 'Y',
+        'B' => return 'Z',
+        'C' => return 'X',
+        _ => return ' ',
+    }
+}
+
+fn get_loosing_char(input: char) -> char {
+    match input {
+        'A' => return 'Z',
+        'B' => return 'X',
+        'C' => return 'Y',
+        _ => return ' ',
+    }
+}
+
+fn calculate_score_second_part(input: &str) -> i32 {
+    let my_input = input.chars().nth(2).expect("Failed to get my char from string");
+    let their_char = input.chars().nth(0).expect("Failed to get their char from string");
+
+    // lose
+    if my_input == 'X' {
+        let shape_points = get_loosing_char(their_char) as i32 - 'X' as i32;
+        return shape_points + 1;
+    }
+
+    // tie
+    if my_input == 'Y' {
+        let shape_points = their_char as i32 - 'A' as i32;
+        return 3 + shape_points + 1;
+    }
+
+    let shape_points = get_winning_char(their_char) as i32 - 'X' as i32;
+    shape_points + 1 + 6
 }
 
 fn calculate_score(input: &str) -> i32 {
